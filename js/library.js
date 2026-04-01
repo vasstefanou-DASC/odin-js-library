e9e6b31adac97a2c6d7caff56966f171f7ae68dc
Vasileios Stefanou <vas.stefanou@gmail.com>
2026-04-01 21:50:57 +0300

    Add cards and toggle read and delete buttons

--- js/library.js
+++ js/library.js
@@ -1,6 +1,9 @@
 const myLibrary = [];
 const bookList = document.querySelector(".booklist");
 
+const dialog = document.getElementById("add-book");
+const confirmBtn = dialog.querySelector("#confirm-add-book");
+
 function Book(author,title,pages,read) {
     this.author = author;
     this.title = title;
@@ -10,20 +13,11 @@ function Book(author,title,pages,read) {
     this.rating = addRating();
 
     function addRating() {
-        if (this.read.toLowerCase()==="no") {
-            this.rating = "Not rated yet";
-        } else if (this.read.toLowerCase()==="yes") {
-            this.rating = +prompt("Please enter a book rating (?/10):");
+        if (read.toLowerCase()==="no") {
+            return "Not rated yet";
+        } else if (read.toLowerCase()==="yes") {
+            return +prompt("Please enter a book rating (?/10):");
         }
-    }
-}
-
-function addBookToLibrary(author,title,pages,read) {
-    if (!findBook(author,title)) {
-        let newBook = new Book(author,title,pages,read);
-        myLibrary.push(newBook);
-    } else {
-        alert("Book already in library database");
     }
 }
 
@@ -36,16 +30,58 @@ function findBook(author,title) {
     return false;
 }
 
+function addBookToLibrary(author,title,pages,read) {
+    if (!findBook(author,title)) {
+        let newBook = new Book(author,title,pages,read);
+        myLibrary.push(newBook);
+    } else {
+        alert("Book already in library database");
+    }
+}
+
 function showBooks() {
+    bookList.innerHTML= "";
+    
     for (const book of myLibrary) {
         const card = document.createElement("div");
         card.classList.add("book-description");
         for (const [key,value] of Object.entries(book)) {
-            const info = document.createElement("p");
-            info.classList.add(`${key}`);
-            info.textContent =  `${value}`;
-            card.append(info);
+            if (key !== "id") {
+                const info = document.createElement("p");
+                info.classList.add(`${key}`);
+                info.textContent = `${key.charAt(0).toUpperCase()+key.substring(1)}: ${value}`;
+                card.append(info);
+            }
         }
+        const toggleRead = document.createElement("button");
+        const deleteBook = document.createElement("button");
+        toggleRead.classList.add("card-buttons")
+        deleteBook.classList.add("card-buttons")
+        toggleRead.textContent = "Toggle Read";
+        deleteBook.textContent = "Delete";
+        card.appendChild(toggleRead);
+        card.appendChild(deleteBook);
         bookList.appendChild(card);
     }
 }
+
+console.log(myLibrary);
+
+confirmBtn.addEventListener("click", function() {
+    const author = dialog.querySelector("#author").value;
+    const title = dialog.querySelector("#title").value;
+    const pages = dialog.querySelector("#pages").value;
+    const read = dialog.querySelector("#read").value;
+    addBookToLibrary(author,title,pages,read);
+    showBooks();
+
+    dialog.close();
+    
+    dialog.querySelector("#author").value = "";
+    dialog.querySelector("#title").value = "";
+    dialog.querySelector("#pages").value = "";
+    dialog.querySelector("#read").value = "";
+});
+
+
+
